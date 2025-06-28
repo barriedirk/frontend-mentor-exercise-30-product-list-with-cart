dbb1bf1e937aa49c41f50913713e268491b734b3
Barrie Freyre <barriedirk@yahoo.com>
2025-06-27 22:37:27 -0500

    add angular with tailwindcss 4

--- project/README.md
+++ project/README.md
@@ -1,4 +1,4 @@
-# ProductListProject
+# Project
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.3.
 
--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'product-list-project';
+  protected title = 'project';
 }
